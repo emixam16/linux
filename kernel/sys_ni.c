3b5a7ee83ba508aef41fbda86e971d282d49a76f
Maxime Bélair <maxime.belair@canonical.com>
2025-06-03 15:39:46 +0200

    Wire up lsm_set_self_policy and lsm_set_system_policy syscalls
    Add support for the new lsm_set_self_policy and lsm_set_system_policy
    syscalls, providing a unified API for loading and modifying LSM
    policies, respectively for the user and for the whole system without
    requiring the LSM’s pseudo-filesystem.
    
    Benefits:
      - Works even if the LSM pseudo-filesystem isn’t mounted or available
        (e.g. in containers)
      - Offers a logical and unified interface rather than multiple
        heterogeneous pseudo-filesystems.
      - Avoids overhead of other kernel interfaces for better efficiency
    
    Signed-off-by: Maxime Bélair <maxime.belair@canonical.com>

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -172,6 +172,8 @@ COND_SYSCALL_COMPAT(fadvise64_64);
 COND_SYSCALL(lsm_get_self_attr);
 COND_SYSCALL(lsm_set_self_attr);
 COND_SYSCALL(lsm_list_modules);
+COND_SYSCALL(lsm_set_self_policy);
+COND_SYSCALL(lsm_set_system_policy);
 
 /* CONFIG_MMU only */
 COND_SYSCALL(swapon);
--- security/lsm_syscalls.c
+++ security/lsm_syscalls.c
@@ -118,3 +118,15 @@ SYSCALL_DEFINE3(lsm_list_modules, u64 __user *, ids, u32 __user *, size,
 
 	return lsm_active_cnt;
 }
+
+SYSCALL_DEFINE5(lsm_set_self_policy, u32, lsm_id, u32, op, void __user *, buf,
+		u32 __user *, size, u32, flags)
+{
+	return 0;
+}
+
+SYSCALL_DEFINE5(lsm_set_system_policy, u32, lsm_id, u32, op, void __user *, buf,
+		u32 __user *, size, u32, flags)
+{
+	return 0;
+}
